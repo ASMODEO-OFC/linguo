aaa514dd3c2ce8937ac9ab5aee2b622823fa8cdd
ASMODEO-OFC <85648496+ASMODEO-OFC@users.noreply.github.com>
2021-06-09 23:14:21 -0300

    Create desmenu.js

---
+++ src/desmenu.js
@@ -0,0 +1,18 @@
+const desmenu = (prefix, pushname) => {
+    return `
+*COMANDOS DE DESCARGAS*
+ 
+- ${prefix}play
+  Coloca el nombre de la cancion. O el link del video de YouTube
+- ${prefix}ytmp3
+  Coloca el link del video de YouTube
+- ${prefix}ytmp4
+  Coloca el link del video de YouTube
+    
+_*play tiene un limite de, si llega a su limite se restablecera dentro de 24 horas_
+     
+⸸𖤐𝐀𝐒𝐌𝐎𝐃𝐄𝐎𖤐⸸ᴼᶠᶜ`
+
+}
+
+exports.desmenu = desmenu
