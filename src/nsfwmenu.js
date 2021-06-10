4b9cdf4fd6d87bfc0f728982de9a312b5d04cc8b
ASMODEO-OFC <85648496+ASMODEO-OFC@users.noreply.github.com>
2021-06-09 23:45:17 -0300

    Create nsfwmenu.js

---
+++ src/nsfwmenu.js
@@ -0,0 +1,18 @@
+const nsfwmenu = (prefix, pushname) => {
+    return `*COMANDOS PARA EL MODO NSFW*
+*NSFW 🔥*
+
+- ${prefix}nsfwbobs
+- ${prefix}nsfwsidebobs
+- ${prefix}nsfwahegao
+- ${prefix}nsfwfeets
+
+_El bot nesecita admin y tener activado el modo NSFW_\n _Digita_\n ${prefix}*nsfw 1*
+
+Algunas funciones fueron eliminadas por errores en los servidores de origen
+
+⸸𖤐𝐀𝐒𝐌𝐎𝐃𝐄𝐎𖤐⸸ᴼᶠᶜ`
+
+}
+
+exports.nsfwmenu = nsfwmenu
