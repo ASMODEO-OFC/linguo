26311bafb36f7f24748e33e0000b187bc1cebd56
ASMODEO-OFC <85648496+ASMODEO-OFC@users.noreply.github.com>
2021-06-10 00:01:16 -0300

    Create welmenu.js

---
+++ src/welmenu.js
@@ -0,0 +1,20 @@
+const welmenu = (prefix, pushname) => {
+    return `
+*Comandos De Bienvenida 👋*
+
+*PARA ACTIVAR LA BIENVENIDA*
+
+- ${prefix}welcome 1
+
+*PARA DESACTIVAR LA BIENVENIDA*
+
+- ${prefix}welcome 0
+
+_Para activar este comando el bot necesita tener admin_
+
+⸸𖤐𝐀𝐒𝐌𝐎𝐃𝐄𝐎𖤐⸸ᴼᶠᶜ
+`
+
+}
+
+exports.welmenu = welmenu
