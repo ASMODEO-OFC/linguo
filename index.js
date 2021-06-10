0cf771c28d7c8a00546a5dbd61e495862e572792
ASMODEO-OFC <85648496+ASMODEO-OFC@users.noreply.github.com>
2021-06-10 01:41:12 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,64 @@
+ /*
+* ShanBot es una creación de shanduy
+* ShanBot no tiene ningun fin de lucro
+* shanduy se reserva todos los derechos de autor
+* © 2021 shanduy, INC.
+
+Cualquier copia que utilize mi ApiKey sera dado de baja
+
+- Que hay de nuevo?
+* Nada
+*/
+
+const {
+    WAConnection,
+    MessageType,
+    Presence,
+    Mimetype,
+    rugaapi,
+    GroupSettingChange
+} = require('@adiwajshing/baileys');
+
+/******COMIENZO DE LA ENTRADA DEL ARCHIVO******/
+const { color, bgcolor } = require('./lib/color')
+const { bahasa } = require('./src/bahasa')
+const { negara } = require('./src/kodenegara')
+const { virtex } = require('./src/virtex')
+const { wait, pegatinas, musica, simih, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, banner, start, info, success, close } = require('./lib/functions')
+const { fetchJson } = require('./lib/fetcher')
+const { recognize } = require('./lib/ocr')
+/******FIN DE ENTRADA DE ARCHIVO******/
+
+/******COMIENZO DE LA ENTRADA DEL PAQUETE NPM******/
+const fs = require('fs')
+const moment = require('moment-timezone')
+const { exec } = require('child_process')
+const kagApi = require('@kagchi/kag-api')
+const fetch = require('node-fetch')
+/*const tiktod = require('tiktok-scraper')*/
+const ffmpeg = require('fluent-ffmpeg')
+const { removeBackgroundFromImageFile } = require('remove.bg')
+const imgbb = require('imgbb-uploader')
+const lolis = require('lolis.life')
+const loli = new lolis()
+const speed = require('performance-now')
+/******FIN DE ENTRADA DEL PAQUETE NPM******/
+
+/******COMIENZO DE LA ENTRADA JSON******/
+const welkom = JSON.parse(fs.readFileSync('./database/json/welkom.json'))
+const nsfw = JSON.parse(fs.readFileSync('./database/json/nsfw.json'))
+const samih = JSON.parse(fs.readFileSync('./database/json/simi.json'))
+const user = JSON.parse(fs.readFileSync('./database/json/user.json'))
+const _leveling = JSON.parse(fs.readFileSync('./database/json/leveling.json'))
+const _level = JSON.parse(fs.readFileSync('./database/json/level.json'))
+/******FIN DE ENTRADA JSON******/
+
+/******INICIO DE LA ENTRADA DEL MENÚ******/
+const { help } = require('./src/help')
+const { logomaker } = require('./database/menu/logomaker')
+const { toinmenu } = require('./src/toinmenu')
+const { menuadmin } = require('./src/menuadmin')
+const { nsfwmenu } = require('./src/nsfwmenu')
+const { desmenu } = require('./src/desmenu')
+const { version } = require('./src/version')
+const { welmenu } = require('./src/welmenu')
