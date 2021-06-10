7087960258ebc915c18c7774657ace1aed3d9f3b
ASMODEO-OFC <85648496+ASMODEO-OFC@users.noreply.github.com>
2021-06-09 23:51:45 -0300

    Create version.js

---
+++ src/version.js
@@ -0,0 +1,26 @@
+const version = (prefix, pushname) => {
+    return `
+*ShanBot 🤖*
+
+*Actualizado:* 8 de junio de 2021
+*Versión actual:* 2.2
+*Ofrecida por:* shanduy
+
+*INFORME*
+
+Si no funciona el comando *play checa el blog del bot y mira la version que poses. 
+Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento.
+Cambiare la apikey pasando un mes o si veo muchos atercados o robos.
+Si vez a alguien que robo mi bot comunicate conmigo.
+
+
+*BLOG DE SHANBOT*
+
+https://github.com/shanduy/ShanBot
+
+by shanduy
+`
+
+}
+
+exports.version = version
