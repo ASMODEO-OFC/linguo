b7bb9d49e4da0b638fe6b69c99c3fc088936265d
ASMODEO-OFC <85648496+ASMODEO-OFC@users.noreply.github.com>
2021-06-10 03:06:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
- /*Nenne
-*
-*
-*
-*
-
-
-
-
-
-
+/*
+* ShanBot es una creación de shanduy
+* ShanBot no tiene ningun fin de lucro
+* shanduy se reserva todos los derechos de autor
+* © 2021 shanduy, INC.
+Cualquier copia que utilize mi ApiKey sera dado de baja
+- Que hay de nuevo?
+* Nada
+*/
 
 const {
     WAConnection,
