0911f2dd643a82d31382f582dbe970e087a7a122
ASMODEO-OFC <85648496+ASMODEO-OFC@users.noreply.github.com>
2021-06-09 23:16:54 -0300

    Update desmenu.js

--- src/desmenu.js
+++ src/desmenu.js
@@ -2,14 +2,14 @@ const desmenu = (prefix, pushname) => {
     return `
 *COMANDOS DE DESCARGAS*
  
-- ${prefix}play
+- ${prefix}*play*
   Coloca el nombre de la cancion. O el link del video de YouTube
-- ${prefix}ytmp3
+- ${prefix}*ytmp3*
   Coloca el link del video de YouTube
-- ${prefix}ytmp4
+- ${prefix}*ytmp4*
   Coloca el link del video de YouTube
     
-_*play tiene un limite de, si llega a su limite se restablecera dentro de 24 horas_
+_*play tiene un limite de, si llega a su limite se restablecera dentro de 24 horas*_
      
 ⸸𖤐𝐀𝐒𝐌𝐎𝐃𝐄𝐎𖤐⸸ᴼᶠᶜ`
 
