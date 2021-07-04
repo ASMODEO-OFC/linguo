4ef34f99f9706ab0b6556a3123d3268373eb1fa2
ASMODEO-OFC <85648496+ASMODEO-OFC@users.noreply.github.com>
2021-07-03 21:45:31 -0300

    Create cekvip.js

---
+++ src/cekvip.js
@@ -0,0 +1,15 @@
+const cekvip = () => { 
+	return `           
+──────────────────
+*Nombre del bot* :  ╚»★𝗟𝗶𝗻𝗴𝘂𝗼★«╝
+──────────────────
+        『 *𝐕𝐈𝐏 𝐔𝐒𝐄𝐑* 』
+──────────────────
+• *Status*    : *ACTIVO*
+────────────────── 
+*Status Bot:* *Online*
+──────────────────
+
+*ERES USUARIO VIP, FELICIDADES! 👌🏼😌`
+}
+exports.cekvip = cekvip
