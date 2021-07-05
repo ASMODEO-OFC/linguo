c96e522879936e00f2bf24de0ce7b50750b9c4a9
ASMODEO-OFC <85648496+ASMODEO-OFC@users.noreply.github.com>
2021-07-05 14:37:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -209,7 +209,7 @@ async function starts() {
 				} catch {
 					ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 				}
-				teks = `𝘼𝙙𝙞𝙤́𝙨 @${num.split('@')[0]} 👋😑`
+				teks = `se fue @${num.split('@')[0]} 👋😑`
 				let buff = await getBuffer(ppimg)
 				client.sendMessage(mdata.id, buff, MessageType.image, {caption: teks, contextInfo: {"mentionedJid": [num]}})
 			}
