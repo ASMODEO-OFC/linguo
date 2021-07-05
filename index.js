b716d35cbbddce0793bbad030e0ef767e00e2e85
ASMODEO-OFC <85648496+ASMODEO-OFC@users.noreply.github.com>
2021-07-05 12:19:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -778,11 +778,6 @@ async function starts() {
 					yeh = `https://chat.whatsapp.com/${linkgc}\n\nLink Del Grupo *${groupName}*`
 					client.sendMessage(from, yeh, text, {quoted: mek, detectLinks: false})
 					break
-                case 'qrcode':
-                buff = await getBuffer(`https://api.qrserver.com/v1/create-qr-code/?data=${body.slice(8)}&size=1080%C3%971080`)
-				client.sendMessage(from, buff, image, {quoted: mek})
-				break
-		          		
 			case 'closegc':
 					client.updatePresence(from, Presence.composing) 
 					if (!isGroup) return reply(mess.only.group)
@@ -809,77 +804,77 @@ async function starts() {
 					client.groupSettingChange (from, GroupSettingChange.messageSend, false)
 					client.sendMessage(from, open, text, {quoted: mek})
 					break
-				case 's':
-				case 'stk':
-				case 'stiker':
-				case 'sticker':
-					if ((isMedia && !mek.message.videoMessage || isQuotedImage) && args.length == 0) {
-						const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
-						const media = await client.downloadAndSaveMediaMessage(encmedia)
-						ran = getRandom('.webp')
-						await ffmpeg(`./${media}`)
-							.input(media)
-							.on('start', function (cmd) {
-								console.log(`Started : ${cmd}`)
-							})
-							.on('error', function (err) {
-								console.log(`Error : ${err}`)
-								fs.unlinkSync(media)
-								reply(mess.error.stick)
-							})
-							.on('end', function () {
-								console.log('Finish')
-								client.sendMessage(from, fs.readFileSync(ran), sticker, {quoted: mek})
-								fs.unlinkSync(media)
-								fs.unlinkSync(ran)
-							})
-							.addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
-							.toFormat('webp')
-							.save(ran)
-					} else if ((isMedia && mek.message.videoMessage.seconds < 11 || isQuotedVideo && mek.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.seconds < 11) && args.length == 0) {
-						const encmedia = isQuotedVideo ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
-						const media = await client.downloadAndSaveMediaMessage(encmedia)
-						ran = getRandom('.webp')
-						reply(mess.only.pegatina)
-						await ffmpeg(`./${media}`)
-							.inputFormat(media.split('.')[1])
-							.on('start', function (cmd) {
-								console.log(`Started : ${cmd}`)
-							})
-							.on('error', function (err) {
-								console.log(`Error : ${err}`)
-								fs.unlinkSync(media)
-								tipe = media.endsWith('.mp4') ? 'video' : 'gif'
-								reply(`❌ Falla al momento de convertir en sticker ❌`)
-							})
-							.on('end', function () {
-								console.log('Finish')
-								client.sendMessage(from, fs.readFileSync(ran), sticker, {quoted: mek})
-								fs.unlinkSync(media)
-								fs.unlinkSync(ran)
-							})
-							.addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
-							.toFormat('webp')
-							.save(ran)
-					} else if ((isMedia || isQuotedImage) && args[0] == 'nobg') {
-						const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
-						const media = await client.downloadAndSaveMediaMessage(encmedia)
-						ranw = getRandom('.webp')
-						ranp = getRandom('.png')
-						reply(mess.wait)
-						keyrmbg = 'Your-ApiKey'
-						await removeBackgroundFromImageFile({path: media, apiKey: keyrmbg.result, size: 'auto', type: 'auto', ranp}).then(res => {
-							fs.unlinkSync(media)
-							let buffer = Buffer.from(res.base64img, 'base64')
-							fs.writeFileSync(ranp, buffer, (err) => {
-								if (err) return reply('❌ Ocurrió un error, intente mas tarde ❌')
-							})
-							exec(`ffmpeg -i ${ranp} -vcodec libwebp -filter:v fps=fps=20 -lossless 1 -loop 0 -preset default -an -vsync 0 -s 512:512 ${ranw}`, (err) => {
-								fs.unlinkSync(ranp)
-								if (err) return reply(mess.error.stick)
-								client.sendMessage(from, fs.readFileSync(ranw), sticker, {quoted: mek})
-							})
-						})
+	  case 's':
+	  case 'stk':
+	  case 'stiker':
+	  case 'sticker':
+		if ((isMedia && !mek.message.videoMessage || isQuotedImage) && args.length == 0) {
+		const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
+		const media = await client.downloadAndSaveMediaMessage(encmedia)
+		ran = getRandom('.webp')
+		await ffmpeg(`./${media}`)
+		.input(media)
+		.on('start', function (cmd) {
+		console.log(`Started : ${cmd}`)
+		})
+		.on('error', function (err) {
+		console.log(`Error : ${err}`)
+		fs.unlinkSync(media)
+		reply(mess.error.stick)
+		})
+		.on('end', function () {
+		console.log('Finish')
+		client.sendMessage(from, fs.readFileSync(ran), sticker, {quoted: mek})
+		fs.unlinkSync(media)
+		fs.unlinkSync(ran)
+		})
+		.addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+		.toFormat('webp')
+		.save(ran)
+		} else if ((isMedia && mek.message.videoMessage.seconds < 11 || isQuotedVideo && mek.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.seconds < 11) && args.length == 0) {
+		const encmedia = isQuotedVideo ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
+		const media = await client.downloadAndSaveMediaMessage(encmedia)
+		ran = getRandom('.webp')
+		reply(mess.only.pegatina)
+		await ffmpeg(`./${media}`)
+		.inputFormat(media.split('.')[1])
+		.on('start', function (cmd) {
+		console.log(`Started : ${cmd}`)
+		})
+		.on('error', function (err) {
+		console.log(`Error : ${err}`)
+		fs.unlinkSync(media)
+		tipe = media.endsWith('.mp4') ? 'video' : 'gif'
+		reply(`❌ Falla al momento de convertir en sticker ❌`)
+		})
+		.on('end', function () {
+		console.log('Finish')
+		client.sendMessage(from, fs.readFileSync(ran), sticker, {quoted: mek})
+		fs.unlinkSync(media)
+		fs.unlinkSync(ran)
+		})
+		.addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+		.toFormat('webp')
+		.save(ran)
+		} else if ((isMedia || isQuotedImage) && args[0] == 'nobg') {
+		const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
+		const media = await client.downloadAndSaveMediaMessage(encmedia)
+		ranw = getRandom('.webp')
+		ranp = getRandom('.png')
+		reply(mess.wait)
+		keyrmbg = 'Your-ApiKey'
+		await removeBackgroundFromImageFile({path: media, apiKey: keyrmbg.result, size: 'auto', type: 'auto', ranp}).then(res => {
+		fs.unlinkSync(media)
+		let buffer = Buffer.from(res.base64img, 'base64')
+		fs.writeFileSync(ranp, buffer, (err) => {
+		if (err) return reply('❌ Ocurrió un error, intente mas tarde ❌')
+		})
+		exec(`ffmpeg -i ${ranp} -vcodec libwebp -filter:v fps=fps=20 -lossless 1 -loop 0 -preset default -an -vsync 0 -s 512:512 ${ranw}`, (err) => {
+		fs.unlinkSync(ranp)
+		if (err) return reply(mess.error.stick)
+		client.sendMessage(from, fs.readFileSync(ranw), sticker, {quoted: mek})
+		})
+		})
 		/*} else if ((isMedia || isQuotedImage) && colors.includes(args[0])) {
 		const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
 		const media = await client.downloadAndSaveMediaMessage(encmedia)
