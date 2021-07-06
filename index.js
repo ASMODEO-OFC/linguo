78536140315533ee133a2c67de334babc51ad466
ASMODEO-OFC <85648496+ASMODEO-OFC@users.noreply.github.com>
2021-07-06 01:31:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1171,6 +1171,28 @@ async function starts() {
                 const pcptgay = porcentagemcpsgay[Math.floor(Math.random() * porcentagemcpsgay.length)]
 		client.sendMessage(from, foto, MessageType.image, {quoted: mek, caption: `Su porcentaje de gay es: ${pcptgay}`})
                 break
+          case 'fiel':
+                if (!isGroup) return reply(mess.only.group)
+                if (mek.message.extendedTextMessage === undefined || mek.message.extendedTextMessage === null) return
+                mentioned = mek.message.extendedTextMessage.contextInfo.mentionedJid
+                if (args.length < 0) return reply('Menciona a alguien')
+                pisk = await client.getProfilePicture(mentioned[0])
+                foto = await getBuffer(pisk)
+                var porcentagemcpsfiel = ["1%", `2%`, `3%`, `4%`, `5%`, `6%`, `7%`, `8%`, `9%`, `10%`, `11%`, `12%`,`13%`, `14%`, `15%`, `16%`, `17%`, `18%`, `19%`, `20%`, `21%`, `22`, `23%`, `24%`, `25%`, `26%`, `27%`, `28%`, `27%`, `28%`, `29%`, `30%`, `31%`, `32%`, `33%`, `34%`, `35%`, `36%`, `37%`, `38%`, `39%`, `40%`, `41%`, `42%`, `43%`, `44%`, `45%`, `46%`, `47%`, `48%`, `49%`, `50%`, `51%`, `52%`, `53%`, `54%`, `55%`, `56%`, `57%`, `58%`, `59%`, `60%`, `61%`, `62%`, `63%`, `64%`, `65%`, `66%`, `67%`, `68%`, `69%`, `70%`, `71%`, `72%`, `73%`, `74%`, `75%`, `76%`, `77%`, `78%`, `79%`, `80%`, `81%`, `82%`, `85%`, `84%`, `85%`, `86%`, `87%`, `88%`, `89%`, `90%`, `91%`, `92%`, `93%`, `94%`, `95%`, `96%`, `97%`, `98%`, `99%`, `100%`]
+                const pcptfiel = porcentagemcpsfiel[Math.floor(Math.random() * porcentagemcpsfiel.length)]                
+                client.sendMessage(from, foto, MessageType.image, {quoted: mek, caption: `Su porcentaje de puta es: ${pcptfiel}`})
+                break
+          case 'cogible':
+                if (!isGroup) return reply(mess.only.group)
+                if (mek.message.extendedTextMessage === undefined || mek.message.extendedTextMessage === null) return
+                mentioned = mek.message.extendedTextMessage.contextInfo.mentionedJid
+                if (args.length < 0) return reply('Menciona a alguien')
+                pisk = await client.getProfilePicture(mentioned[0])
+                foto = await getBuffer(pisk)
+                var porcentagemcpscog = ["1%", `2%`, `3%`, `4%`, `5%`, `6%`, `7%`, `8%`, `9%`, `10%`, `11%`, `12%`,`13%`, `14%`, `15%`, `16%`, `17%`, `18%`, `19%`, `20%`, `21%`, `22`, `23%`, `24%`, `25%`, `26%`, `27%`, `28%`, `27%`, `28%`, `29%`, `30%`, `31%`, `32%`, `33%`, `34%`, `35%`, `36%`, `37%`, `38%`, `39%`, `40%`, `41%`, `42%`, `43%`, `44%`, `45%`, `46%`, `47%`, `48%`, `49%`, `50%`, `51%`, `52%`, `53%`, `54%`, `55%`, `56%`, `57%`, `58%`, `59%`, `60%`, `61%`, `62%`, `63%`, `64%`, `65%`, `66%`, `67%`, `68%`, `69%`, `70%`, `71%`, `72%`, `73%`, `74%`, `75%`, `76%`, `77%`, `78%`, `79%`, `80%`, `81%`, `82%`, `85%`, `84%`, `85%`, `86%`, `87%`, `88%`, `89%`, `90%`, `91%`, `92%`, `93%`, `94%`, `95%`, `96%`, `97%`, `98%`, `99%`, `100%`]
+                const pcptcog = porcentagemcpscog[Math.floor(Math.random() * porcentagemcpscog.length)]                
+                client.sendMessage(from, foto, MessageType.image, {quoted: mek, caption: `Su porcentaje de puta es: ${pcptcog}`})
+                break
           case 'puta':
                 if (!isGroup) return reply(mess.only.group)
                 if (mek.message.extendedTextMessage === undefined || mek.message.extendedTextMessage === null) return
