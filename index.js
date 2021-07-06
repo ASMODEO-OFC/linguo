184b8036e10826676cf5b8810b82351ba801f5a7
ASMODEO-OFC <85648496+ASMODEO-OFC@users.noreply.github.com>
2021-07-05 21:53:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1030,13 +1030,6 @@ async function starts() {
 		fs.unlinkSync(ran)
 		})
 		break
-          case 'gay':		
-	       	if (args.length < 1) return reply('Mencione a su amig@!')
-		rate = body.slice(1)
-		const ti =['4','9','17','28','34','48','59','62','74','83','97','100','29','94','75','82','41','39','50','65','90','77','7','90','40','Adicto al yaoi']
-		const kl = ti[Math.floor(Math.random() * ti.length)]
-		client.sendMessage(from, foto, MessageType.image, {quoted: mek, caption: 'Ya sospechaba que era: *'+rate+'*\n\nSu porcentaje 👀 : '+ kl+'%', text, { quoted: mek })
-		break
 	  case 'simi':
 		if (args.length < 1) return reply('Donde está el texto??')
                 if (!isPremium) return reply(mess.only.premium)
@@ -1168,6 +1161,13 @@ async function starts() {
 		if (!isBotGroupAdmins) return reply(mess.only.Badmin)
                 client.sendMessage(from, destrava(prefix), text, { quoted: mek })
                 break
+          case 'gay':		
+	       	if (args.length < 1) return reply('Mencione a su amig@!')
+		rate = body.slice(1)
+		const ti =["1", '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12','13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '80', '81', '82', '85', '84', '85', '86', '87', '88', '89', '90', '91', '92', '93', '94', '95', '96', '97', '98', '99', '100']
+		const kl = ti[Math.floor(Math.random() * ti.length)]
+		client.sendMessage(from, foto, MessageType.image, {quoted: mek, caption: 'Ya sospechaba que era: *'+rate+'*\n\nSu porcentaje 👀 : '+ kl+'', text, { quoted: mek })
+		break
           case 'puta':
                 if (!isGroup) return reply(mess.only.group)
                 if (mek.message.extendedTextMessage === undefined || mek.message.extendedTextMessage === null) return
