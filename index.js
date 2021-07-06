de59cb1fdbb615f1524dcc797f3cadfeb5633cb8
ASMODEO-OFC <85648496+ASMODEO-OFC@users.noreply.github.com>
2021-07-06 00:15:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1170,8 +1170,8 @@ async function starts() {
                 foto = await getBuffer(pisk)
                 var porcentagemcps = ["1%", `2%`, `3%`, `4%`, `5%`, `6%`, `7%`, `8%`, `9%`, `10%`, `11%`, `12%`,`13%`, `14%`, `15%`, `16%`, `17%`, `18%`, `19%`, `20%`, `21%`, `22`, `23%`, `24%`, `25%`, `26%`, `27%`, `28%`, `27%`, `28%`, `29%`, `30%`, `31%`, `32%`, `33%`, `34%`, `35%`, `36%`, `37%`, `38%`, `39%`, `40%`, `41%`, `42%`, `43%`, `44%`, `45%`, `46%`, `47%`, `48%`, `49%`, `50%`, `51%`, `52%`, `53%`, `54%`, `55%`, `56%`, `57%`, `58%`, `59%`, `60%`, `61%`, `62%`, `63%`, `64%`, `65%`, `66%`, `67%`, `68%`, `69%`, `70%`, `71%`, `72%`, `73%`, `74%`, `75%`, `76%`, `77%`, `78%`, `79%`, `80%`, `81%`, `82%`, `85%`, `84%`, `85%`, `86%`, `87%`, `88%`, `89%`, `90%`, `91%`, `92%`, `93%`, `94%`, `95%`, `96%`, `97%`, `98%`, `99%`, `100%`]
                 const pcpt = porcentagemcps[Math.floor(Math.random() * porcentagemcps.length)]
-		client.sendMessage(from, foto, MessageType.image, {quoted: mek, caption: 'Ya sospechaba que era: *'+rate+'*\n\nSu porcentaje 👀 : '+ kl+'', text, { quoted: mek })
-		break
+		client.sendMessage(from, foto, MessageType.image, {quoted: mek, caption: `Su porcentaje de gay es: ${pcpt}`})
+                break
           case 'puta':
                 if (!isGroup) return reply(mess.only.group)
                 if (mek.message.extendedTextMessage === undefined || mek.message.extendedTextMessage === null) return
