6053d9d9ec3f9ec2a824cb8d5aa9c3cdb3dd00f8
ASMODEO-OFC <85648496+ASMODEO-OFC@users.noreply.github.com>
2022-02-24 09:25:55 -0300

    Create life.js

---
+++ src/life.js
@@ -0,0 +1,13 @@
+const version = (prefix, pushname) => {
+    return `
+*╚»★𝗟𝗶𝗻𝗴𝘂𝗼★«╝ by ⸸𖤐𝐀𝐒𝐌𝐎𝐃𝐄𝐎𖤐⸸ᴼᶠᶜ*
+*Actualizado:* 11 de junio de 2021
+*Versión actual:* 1.9.7
+*Ofrecida por:* © 2021 ⸸𖤐𝐀𝐒𝐌𝐎𝐃𝐄𝐎𖤐⸸ᴼᶠᶜ
+*INFORME*
+Se iran corrigiendo, mejorando y agregando nuevos comandos
+`
+
+}
+
+exports.version = version
