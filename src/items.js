c8f59b1e83df8114714bb57336a4bed4e0f42f5e
ASMODEO-OFC <85648496+ASMODEO-OFC@users.noreply.github.com>
2022-03-04 14:56:51 -0300

    Update items.js

--- src/items.js
+++ src/items.js
@@ -3,10 +3,19 @@ const items = (prefix, pushname) => {
 
 𝙄́𝙏𝙀𝙈𝙎 𝙀𝙎𝙋𝙀𝘾𝙄𝘼𝙇𝙀𝙎 
 
-𝗟𝗩.1:
+🅛🅥.1:
 
 ❂ Clear Sky Wheat Field (Infected, Hemp, Fishing)
 ❂ Fall Forest (Infected x2, Fishing, Hemp, Logging, Minning)
+❂ Summer Rain Highway (Infected, Minning, Fishing)
+
+🅛🅥.2:
+
+❂ Assiryan Hills (Hemp, Logging, Minning)
+❂ Assiryan Grassfield (Hemp, Logging, Minning)
+❂ Blade Hunter Base (Infected, Hemp, Minning)
+
+🅛🅥.3
 `
 
 }
